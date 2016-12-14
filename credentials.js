8e8f93ed0868a7289b2f971d67506320645d328d
Jark1 <jeremy.krinsley@gmail.com>
2016-12-14 11:45:24 -0500

    adding file

---
+++ credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key:'vfztSi8IW125qS2mFIzaQ38Az',
+	consumer_secret:'US7TvslMGVpltfhUWtYG2bTHJbzX3UCgp44wvN8ZstHJjWrZqx',
+	access_token_key:'34075741-egPTenrL5HKExI7qIxnDSvtbOGixVOH7HQNHG4bwL',
+	access_token_secret:'Qm6dNrZHaHQXnIep2MdGvurfX7a1DybBgg8ceXOvL37xY'
+};
--- procfile
+++ procfile
@@ -1,1 +1,1 @@
-web: node twitter.js
+web: node app.js
